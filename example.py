8065b81dce01d2fc0945715220f21a3df965302b
volkserg <volksergg@yandex.ru>
2019-03-13 17:23:52 +0300

    example

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 from lab2 import *
 
 
-# s = Solver(0.1, 0.1, 10) # a,b,n
-# print(s.calc_VarN())
-# print(s.q)
+s = Solver(0.1, 0.4, 100) # a,b,n
+print(s.calc_VarN())
+print(s.q)
 
 
-g = Graphics()
+# g = Graphics()
 
 # g.draw_N_Nx(0.1, 0.2, 2)
 
 # g.draw_VarN_VarNx(0.1, 0.2, 2)
 
-g.draw_special_pi(0.1, 0.1, 3)
+# g.draw_special_pi(0.1, 0.1, 3)
